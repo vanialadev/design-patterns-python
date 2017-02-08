c42693fc2f9a8c05f18e891d77f2e793cb7dd73a
Vania Almeida <vanialadev@gmail.com>
2017-02-08 14:29:06 -0300

    orientado a objetos

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,16 +1,8 @@
-from impostos import calcula_ISS, calcula_ICMS
+from impostos import ISS, ICMS
 
 class Calculador_de_imposto(object):
 
     def realiza_calculo(self, orcamento, imposto):
-
-        # if imposto == 'ISS':
-        #     # imposto_calculado = orcamento.valor * 0.1
-        #     imposto_calculado = calcula_ISS(orcamento)
-        #
-        # elif imposto == 'ICMS':
-        #     # imposto_calculado = orcamento.valor * 0.06
-        #     imposto_calculado = calcula_ICMS(orcamento)
 
         imposto_calculado = imposto(orcamento)
 
@@ -25,9 +17,8 @@ if __name__== '__main__':
 
     orcamento = Orcamento(500)
 
-    # calculador.realiza_calculo(orcamento, 'ISS')
-    calculador.realiza_calculo(orcamento, calcula_ISS)
-    calculador.realiza_calculo(orcamento, calcula_ICMS)
+    calculador.realiza_calculo(orcamento, ISS().calcula())
+    calculador.realiza_calculo(orcamento, ICMS().calcula())
 
-    #pattern Strategy
+    #orientado a objetos
 
--- impostos.py
+++ impostos.py
@@ -1,6 +1,11 @@
 
-def calcula_ISS(orcamento):
-    return orcamento.valor * 0.1
+class ISS(object):
 
-def calcula_ICMS(orcamento):
-    return orcamento.valor * 0.06
+    def calcula(orcamento):
+        return orcamento.valor * 0.1
+
+
+class ICMS(object):
+
+    def calcula(orcamento):
+        return orcamento.valor * 0.06
