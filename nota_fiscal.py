0ceb78cb338fec3f8372d4c85bd2e4760b10590d
Vania Almeida <vanialadev@gmail.com>
2017-02-14 17:25:32 -0300

    design patterns observer

--- nota_fiscal.py
+++ nota_fiscal.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 # nota_fiscal.py
 from datetime import date
+# from observadores import imprime, envia_por_email, salva_no_banco
 
 class Item(object):
 
@@ -18,7 +19,7 @@ class Item(object):
 
 class Nota_fiscal(object):
 
-    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes=''):
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes='', observadores=[]):
         self.__razao_social = razao_social
         self.__cnpj = cnpj
         self.__data_de_emissao = data_de_emissao
@@ -26,6 +27,14 @@ class Nota_fiscal(object):
             raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
         self.__detalhes = detalhes
         self.__itens = itens
+
+        for observador in observadores:
+            observador(self)
+
+
+        # imprime(self)
+        # envia_por_email(self)
+        # salva_no_banco(self)
 
     @property
     def razao_social(self):
@@ -53,6 +62,7 @@ class Nota_fiscal(object):
 if __name__ == '__main__':
 
     from criador_de_nota_fiscal import Criador_de_nota_fiscal
+    from observadores import imprime, envia_por_email, salva_no_banco
 
     itens=[
         Item(
@@ -65,24 +75,26 @@ if __name__ == '__main__':
         )
     ]
 
+
     nota_fiscal = Nota_fiscal(
         cnpj='012345678901234',
         razao_social='FHSA Limitada',
         itens=itens,
         detalhes='Referente à ...',
-        data_de_emissao=date.today()
+        data_de_emissao=date.today(),
+        observadores=[imprime, envia_por_email, salva_no_banco],
     )
 
     nota_fiscal.mostra()
     # print(nota_fiscal.data_de_emissao)
 
-    nota_fiscal_criada_com_o_builder = (Criador_de_nota_fiscal()
-                                        .com_razao_social('FHSA Limitada')
-                                        .com_cnpj('01234567r8901234')
-                                        .com_itens(itens)
-                                        .constroi()
-
-    )
-
-    nota_fiscal_criada_com_o_builder.mostra()
+    # nota_fiscal_criada_com_o_builder = (Criador_de_nota_fiscal()
+    #                                     .com_razao_social('FHSA Limitada')
+    #                                     .com_cnpj('01234567r8901234')
+    #                                     .com_itens(itens)
+    #                                     .constroi()
+    #
+    # )
+    #
+    # nota_fiscal_criada_com_o_builder.mostra()
 
---
+++ observadores.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+def imprime(nota_fiscal):
+    print('Imprimindo nota fiscal %s' % nota_fiscal.cnpj)
+
+
+def envia_por_email(nota_fiscal):
+    print('Enviando nota fiscal %s' % nota_fiscal.cnpj)
+
+
+def salva_no_banco(nota_fiscal):
+    print('Salvando nota fiscal %s' % nota_fiscal.cnpj)
